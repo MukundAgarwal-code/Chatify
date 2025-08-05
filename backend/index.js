04a38c47b7c12b7caf4b487e160e51c8848c836f
MukundAgarwal-code <agarwalmukund1176@gmail.com>
2025-08-05 15:07:56 +0530

    index.js fix

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
-app.use("trust proxy", 1);
+app.set("trust proxy", 1);
 
 const corsOption={
     origin:'http://localhost:3000',
