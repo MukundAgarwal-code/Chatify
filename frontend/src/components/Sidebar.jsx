7d05b6f98f7828d4bd82f0ba82b1d5c5a7249f14
MukundAgarwal-code <agarwalmukund1176@gmail.com>
2025-09-09 14:37:16 +0530

    search fix 5

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BiSearchAlt2, BiX } from "react-icons/bi";
 import OtherUsers from './OtherUsers';
 import axios from "axios";
